1c6677253c9315ac97e630b0a287cebd60a7dfda
Alex Huang <whuang288@wisc.edu>
2023-05-01 01:16:02 -0500

    THUMOS done

--- condor/check.sh
+++ condor/check.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 
 # TODO: modify this part if needed
-ENVNAME=resize      
+PROJECT=resize_videos
+ENVNAME=resize   
 INPUT_STAGING_DIR=groups/li_group_biostats
-INPUT_TAR=resized_test
+INPUT_TAR=THUMOS14_val_30fps
 
 # Assumptions:
 # 1) code is zipped to "resize_videos.tar.gz"
 set -e
 export PATH
 mkdir $ENVNAME
-tar -xzf /staging/$INPUT_STAGING_DIR/$ENVNAME.tar.gz -C $ENVNAME
+tar -xzf /staging/$INPUT_STAGING_DIR/$PROJECT/$ENVNAME.tar.gz -C $ENVNAME
 . $ENVNAME/bin/activate
-tar -xzf /staging/$INPUT_STAGING_DIR/resize_videos.tar.gz
-tar -xzf /staging/$INPUT_STAGING_DIR/$INPUT_TAR.tar.gz
+tar -xzf /staging/$INPUT_STAGING_DIR/$PROJECT/resize_videos.tar.gz
+tar -xzf /staging/$INPUT_STAGING_DIR/datasets/$INPUT_TAR.tar.gz
 
 # run the extraction
 python check_videos.py -vi ./videos_resized -fps 30
--- condor/resize.sh
+++ condor/resize.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # TODO: modify this part if needed
+PROJECT=resize_videos
 ENVNAME=resize      
 INPUT_STAGING_DIR=groups/li_group_biostats
 INPUT_TAR=THUMOS14_val
@@ -13,10 +14,10 @@ OUTPUT_TAR=resized_val
 set -e
 export PATH
 mkdir $ENVNAME
-tar -xzf /staging/$INPUT_STAGING_DIR/$ENVNAME.tar.gz -C $ENVNAME
+tar -xzf /staging/$INPUT_STAGING_DIR/$PROJECT/$ENVNAME.tar.gz -C $ENVNAME
 . $ENVNAME/bin/activate
-tar -xzf /staging/$INPUT_STAGING_DIR/resize_videos.tar.gz
-tar -xzf /staging/$INPUT_STAGING_DIR/$INPUT_TAR.tar.gz
+tar -xzf /staging/$INPUT_STAGING_DIR/$PROJECT/resize_videos.tar.gz
+tar -xzf /staging/$INPUT_STAGING_DIR/datasets/$INPUT_TAR.tar.gz
 
 # run the extraction
 python resize_videos.py -vi ./$INPUT_FOLDER -vo ./videos_resized  -s 288 -fps 30
